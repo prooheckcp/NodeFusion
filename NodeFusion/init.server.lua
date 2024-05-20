7199cd02784139e447b41ed32be625d756f6b78d
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2024-05-21 00:19:26 +0100

    Change file into folder

--- NodeFusion/init.server.lua
+++ NodeFusion/init.server.lua
@@ -1,7 +1,46 @@
---[[
-plugin.Unloading:Connect(Observer(widgetsEnabled):onChange(function()
-    --enableButton:SetActive(widgetsEnabled:get(false))
-end))    
-]]
+local Widget = require(script.Vendor.PluginComponents.Widget)
+local Toolbar = require(script.Vendor.PluginComponents.Toolbar)
+local ToolbarButton = require(script.Vendor.PluginComponents.ToolbarButton)
+local Fusion = require(script.Vendor.Fusion)
+local Board = require(script.Components.Board)
 
-print("hello girl slay")
+local Value = Fusion.Value
+local OnEvent = Fusion.OnEvent
+local Children = Fusion.Children
+local OnChange = Fusion.OnChange
+
+local widgetsEnabled = Value(false)
+local pluginToolbar = Toolbar {
+    Name = "Example Toolbar"
+}
+local enableButton = ToolbarButton {
+    Toolbar = pluginToolbar,
+
+    ClickableWhenViewportHidden = true,
+    Name = "Examples",
+    ToolTip = "View Example Components",
+    Image = "",
+
+    [OnEvent "Click"] = function()
+        widgetsEnabled:set(not widgetsEnabled:get())
+    end,
+}
+
+Widget {
+    Id = game:GetService("HttpService"):GenerateGUID(),
+    Name = "Component Examples",
+
+    InitialDockTo = Enum.InitialDockState.Right,
+    InitialEnabled = false,
+    ForceInitialEnabled = false,
+    FloatingSize = Vector2.new(250, 200),
+    MinimumSize = Vector2.new(250, 200),
+
+    Enabled = widgetsEnabled,
+    [OnChange "Enabled"] = function(isEnabled)
+        widgetsEnabled:set(isEnabled)
+    end,
+    [Children] = {
+        Board {}
+    },
+}
